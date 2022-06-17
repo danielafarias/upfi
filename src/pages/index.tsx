e8781167458468e74c2b184db96778291fd6b366
Daniela Farias <farias.danicris@gmail.com>
2022-06-16 21:52:30 -0300

    Pagination

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -63,7 +63,15 @@ export default function Home(): JSX.Element {
 
       <Box maxW={1120} px={20} mx="auto" my={20}>
         <CardList cards={formattedData} />
-        {/* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */}
+
+        <Button
+          onClick={() => fetchNextPage()}
+          disabled={!hasNextPage || isFetchingNextPage}
+          type="button"
+          mt={10}
+        >
+          {isFetchingNextPage ? 'Carregando...' : 'Carregar mais'}
+        </Button>
       </Box>
     </>
   );
